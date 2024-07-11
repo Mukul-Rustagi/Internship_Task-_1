fc6343bbdc72575e0da18cb3fd6b9069a3c67074
Mukul-Rustagi <adityarustagi1507@gmail.com>
2024-07-11 12:13:00 +0530

    update uri

--- config/databse.js
+++ config/databse.js
@@ -1,32 +1,21 @@
-// const mongoose=require("mongoose");
-
-// require("dotenv").config();
-
-// exports.connect=()=>{
-//     mongoose.connect(process.env.MONGO_URI,{
-//         useNewUrlParser:true,
-//         useUnifiedTopology:true
-//     })
-
-//     .then(()=>{console.log("DB ka Connection Successful")})
-//     .catch((err)=>{
-//         console.log("DB Conection Issues");
-//         console.error(err);
-//         process.exit(1);
-
-//     });
-// }
-
-const mongoose = require('mongoose');
+const mongoose=require("mongoose");
 
 require("dotenv").config();
-exports.connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
+
+exports.connect=()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+
+    .then(()=>{console.log("DB ka Connection Successful")})
+    .catch((err)=>{
+        console.log("DB Conection Issues");
+        console.error(err);
+        process.exit(1);
+
+    });
+}
+
+
 
